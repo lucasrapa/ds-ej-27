7a6db875ac66571478954857923ce2acaa28419e
lucas <lucasrapaport195@gmail.com>
2023-06-23 20:59:16 -0300

    commit2

--- modelo/index.php
+++ modelo/index.php
@@ -3,7 +3,7 @@ require_once 'modelo/producto';
 require_once 'modelo/proveedores';
 
 $r = new producto();
-$r->Id = 1
+$r->Id = 1;
 $r->Nombre = 'Barra Tybo';
 $r->Codigo = 'rpsa';
 $r->Precio = 1450;
@@ -13,7 +13,7 @@ $p1 = new proveedores();
 $p1->Id = 1;
 $p1->Nombre = 'Segser';
 $p1->CUIL = 20452146471;
-$p1->$direccion = 'Juan Jose Paso 6785'
+$p1->$direccion = 'Juan Jose Paso 6785';
 $p1->$telefono = 3416842356;
 
 
@@ -21,7 +21,7 @@ $p2 = new proveedores();
 $p2->Id = 1;
 $p2->Nombre = 'Food Corner';
 $p2->CUIL = 2077452341;
-$p2->$direccion = 'Oroño 675'
+$p2->$direccion = 'Oroño 675';
 $p2->$telefono = 3415426988;
 
 
@@ -29,7 +29,7 @@ $p3 = new proveedores();
 $p3->Id = 1;
 $p3->Nombre = 'Mex Food';
 $p3->CUIL = 20184623558;
-$p3->$direccion = 'Junin 1544'
+$p3->$direccion = 'Junin 1544';
 $p3->$telefono = 3412389644;
 
 $r->ListaProveedores[] =$p1;
